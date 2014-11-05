a2a09c931df17ca82a744b9c96b81ae5b275a621
pena.luis.89@gmail.com <penal1@citadel.edu>
2014-11-05 19:10:18 -0400

    yes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project::Application.config.secret_token = '12b8dbda0b001452049da701d84e62c34f85d11d9b6b3eaeb8baa354090bc66bc39281c854d99aba356b1f085d21cd76a0672633b7ef48059ff7b8cf1fddca31'
