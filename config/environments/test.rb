346c964dd123f7c2f23c08a7a941bb7dbd8c4845
pena89 <penal1@citadel.edu>
2014-12-14 20:24:09 -0500

    Update test.rb

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,4 @@
-FuelEd::Application.configure do
+MathLab::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
