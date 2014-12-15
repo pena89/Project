86d18e324ca32cff197d36ce61774be407ada548
pena89 <penal1@citadel.edu>
2014-12-14 20:23:09 -0500

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-FuelEd::Application.configure do
+MathLab::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
